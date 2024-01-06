c045ce29e5de0a0a7c8c243aeadd7d08585443fb
reeve <2544141826@qq.com>
2024-01-06 18:58:59 +0800

    更新pitch轴限位逻辑

--- Robot_Hero_top/User/Task/Pitch_task.c
+++ Robot_Hero_top/User/Task/Pitch_task.c
@@ -15,89 +15,92 @@ pitch_t pitch;
 float relative_pitch = 0;
 extern INS_t INS_bottom;
 
-void Pitch_task(void const * argument)
+void Pitch_task(void const *argument)
 {
     pitch_loop_init();
 
-    for(;;)
+    for (;;)
     {
-        pitch.target_speed = pitch_speed_map(rc_ctrl.rc.ch[1], -660, 660, -pitch.speed_max, pitch.speed_max);
-        //pitch_position_judge();
-		pitch_current_give();
+        pitch_position_limit();
+        pitch_current_give();
         osDelay(1);
     }
 }
 
-
 /****************初始化****************/
 void pitch_loop_init()
 {
-    pitch.pid_value[0] = 10;
-    pitch.pid_value[1] = 0;
-    pitch.pid_value[2] = 0;
+    pitch.angle_pid_value[0] = 1;
+    pitch.angle_pid_value[1] = 0;
+    pitch.angle_pid_value[2] = 0;
 
+    pitch.speed_pid_value[0] = 10;
+    pitch.speed_pid_value[1] = 0;
+    pitch.speed_pid_value[2] = 0;
+
+    pitch.target_angle = INS.Roll;
     pitch.target_speed = 0;
     pitch.speed_max = 2000;
 
-    pid_init(&pitch.pid, pitch.pid_value, 100, pitch.speed_max);
+    pid_init(&pitch.pid_angle, pitch.angle_pid_value, 100, 100);
+    pid_init(&pitch.pid_speed, pitch.speed_pid_value, 100, pitch.speed_max);
 }
-
 
 /********************************can1发送电流***************************/
 void pitch_can2_cmd(int16_t v3)
 {
-  uint32_t send_mail_box;
-  CAN_TxHeaderTypeDef tx_header;
-  uint8_t             tx_data[8];
-    
-  tx_header.StdId = 0x1FF;
-  tx_header.IDE   = CAN_ID_STD;//标准帧
-  tx_header.RTR   = CAN_RTR_DATA;//数据帧
-	
-  tx_header.DLC   = 8;		//发送数据长度（字节）
+    uint32_t send_mail_box;
+    CAN_TxHeaderTypeDef tx_header;
+    uint8_t tx_data[8];
 
-  tx_data[0] =         NULL;
-  tx_data[1] =         NULL;
-  tx_data[2] =         NULL;
-  tx_data[3] =         NULL;
-  tx_data[4] = (v3>>8)&0xff;
-  tx_data[5] =    (v3)&0xff;
-  tx_data[6] =         NULL;
-  tx_data[7] =         NULL;
+    tx_header.StdId = 0x1FF;
+    tx_header.IDE = CAN_ID_STD;   // 标准帧
+    tx_header.RTR = CAN_RTR_DATA; // 数据帧
 
-  HAL_CAN_AddTxMessage(&hcan2, &tx_header, tx_data, &send_mail_box);
+    tx_header.DLC = 8; // 发送数据长度（字节）
+
+    tx_data[0] = NULL;
+    tx_data[1] = NULL;
+    tx_data[2] = NULL;
+    tx_data[3] = NULL;
+    tx_data[4] = (v3 >> 8) & 0xff;
+    tx_data[5] = (v3) & 0xff;
+    tx_data[6] = NULL;
+    tx_data[7] = NULL;
+
+    HAL_CAN_AddTxMessage(&hcan2, &tx_header, tx_data, &send_mail_box);
 }
-
 
 /****************PID计算速度并发送电流***************/
 void pitch_current_give()
 {
-    motor_can2[2].set_current = pid_calc(&pitch.pid, pitch.target_speed, -motor_can2[2].rotor_speed);
+    pitch.target_speed = pid_calc(&pitch.pid_angle, pitch.target_angle, INS.Roll);
+    motor_can2[2].set_current = pid_calc(&pitch.pid_speed, pitch.target_speed, -motor_can2[2].rotor_speed);
     pitch_can2_cmd(-motor_can2[2].set_current);
 }
-
 
 /*************pitch速度映射********************/
 int16_t pitch_speed_map(int value, int from_min, int from_max, int to_min, int to_max)
 {
     // 首先将输入值从 [a, b] 映射到 [0, 1] 范围内
-    double normalized_value = (value*1.0 - from_min) / (from_max - from_min);
-    
+    double normalized_value = (value * 1.0 - from_min) / (from_max - from_min);
+
     // 然后将标准化后的值映射到 [C, D] 范围内
     int16_t mapped_value = (int16_t)(to_min + (to_max - to_min) * normalized_value);
-    
+
     return mapped_value;
 }
 
-
 /***************判断pitch位置******************/
-void pitch_position_judge()
+void pitch_position_limit()
 {
     relative_pitch = INS.Roll - INS_bottom.Roll;
-    if(relative_pitch > -30 && relative_pitch < 15){
-        return;
+    if (relative_pitch > -30 && relative_pitch < 15)
+    {
+        pitch.target_angle += rc_ctrl.rc.ch[1] / 660 * 0.5;
     }
-    else{
-        pitch.target_speed = 0;
+    else
+    {
+        return;
     }
 }
--- Robot_Hero_top/User/Task/Pitch_task.h
+++ Robot_Hero_top/User/Task/Pitch_task.h
@@ -6,20 +6,22 @@
 
 typedef struct
 {
-    pid_struct_t pid;            // speed的pid结构体
-    fp32 pid_value[3];           // speed的pid参数
-    fp32 target_speed;           // 目标速度
-    fp32 speed_max;              // 最大速度
+    pid_struct_t pid_angle;  // angle的pid结构体
+    pid_struct_t pid_speed;  // speed的pid结构体
+    fp32 angle_pid_value[3]; // angle的pid参数
+    fp32 speed_pid_value[3]; // speed的pid参数
+    fp32 target_angle;       // 目标pitch
+    fp32 target_speed;       // 目标速度
+    fp32 speed_max;          // 最大速度
 } pitch_t;
 
-
-void Pitch_task(void const * argument);
+void Pitch_task(void const *argument);
 
 // 初始化
 void pitch_loop_init();
 
 /*can1发送电流*/
-void pitch_can1_cmd(int16_t v3);
+void pitch_can2_cmd(int16_t v3);
 
 // PID计算速度并发送电流
 void pitch_current_give();
@@ -27,7 +29,7 @@ void pitch_current_give();
 // pitch速度映射
 int16_t pitch_speed_map(int value, int from_min, int from_max, int to_min, int to_max);
 
-//判断pitch位置
-void pitch_position_judge();
+// 判断pitch位置
+void pitch_position_limit();
 
 #endif
